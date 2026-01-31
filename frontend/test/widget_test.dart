285e0187378c491a7a38f3f8e676c9973536cc6c
itsvetkov1 <i.b.cvetkov@gmail.com>
2026-01-31 19:39:27 +0200

    fix(22-01): update widget test for ProviderProvider requirement
    - Add import for provider_provider.dart
    - Create ProviderProvider in test setup
    - Pass providerProvider to MyApp constructor

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -5,6 +5,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:frontend/main.dart';
 import 'package:frontend/providers/theme_provider.dart';
 import 'package:frontend/providers/navigation_provider.dart';
+import 'package:frontend/providers/provider_provider.dart';
 
 void main() {
   testWidgets('App smoke test', (WidgetTester tester) async {
@@ -15,11 +16,13 @@ void main() {
     // Create providers
     final themeProvider = await ThemeProvider.load(prefs);
     final navigationProvider = await NavigationProvider.load(prefs);
+    final providerProvider = await ProviderProvider.load(prefs);
 
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp(
       themeProvider: themeProvider,
       navigationProvider: navigationProvider,
+      providerProvider: providerProvider,
     ));
 
     // Verify app starts (splash screen should be shown)
